2a1756023a8de685f52434093e49d8a8f5e68f0e
dreonwo <dwheatleyowens@icloud.com>
2021-11-16 20:00:52 -0500

    Update firebase.mjs

--- firebase.mjs
+++ firebase.mjs
@@ -9,13 +9,13 @@ from "https://www.gstatic.com/firebasejs/9.4.1/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBmjzxWY2_7MDAnzB3hV0Wi98PIfp94R1E",
-  authDomain: "dchat-f88d1.firebaseapp.com",
-  projectId: "dchat-f88d1",
-  storageBucket: "dchat-f88d1.appspot.com",
-  messagingSenderId: "42289284703",
-  appId: "1:42289284703:web:4af8e820dadc926a67bdea"
-};
+    apiKey: "AIzaSyBmjzxWY2_7MDAnzB3hV0Wi98PIfp94R1E",
+    authDomain: "dchat-f88d1.firebaseapp.com",
+    projectId: "dchat-f88d1",
+    storageBucket: "dchat-f88d1.appspot.com",
+    messagingSenderId: "42289284703",
+    appId: "1:42289284703:web:4af8e820dadc926a67bdea"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
