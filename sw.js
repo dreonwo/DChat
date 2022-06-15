3c4364fea0273f93813d1e61c0fe7e0af5ae2419
dreonwo <dwheatleyowens@icloud.com>
2022-06-15 19:08:28 -0400

    Edited SW and index

--- sw.js
+++ sw.js
@@ -1,7 +1,46 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      console.log('ServiceWorker registration successful!');
-    }).catch(function(err) {
-      console.log('ServiceWorker registration failed: ', err);
+const CACHE_NAME = "v1.8";
+const OFFLINE_URL = "offline.html";
+
+
+async function cacheOffline(){
+    const cache = await caches.open(CACHE_NAME);
+    cache.add( OFFLINE_URL );
+}
+
+function deleteOldCache(){
+    caches.keys()
+    .then(keys=>{
+        return Promise.all(keys
+            .filter(key=>key!==CACHE_NAME)
+            .map(key=>caches.delete(key))
+        )
     });
-  }
+}
+
+async function onlineOrOffline(req){
+    try {
+        // Always try the network first.
+        return await fetch(req);
+    } catch (error) {
+        console.log(error)
+        // if there is a network error, respond with offline files
+        const cache = await caches.open(CACHE_NAME);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+    }
+}
+
+self.addEventListener("install", (event) => {
+    event.waitUntil( cacheOffline() );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil( deleteOldCache() );
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.mode === "navigate") {
+        event.respondWith( onlineOrOffline(event.request) );
+    }
+});
