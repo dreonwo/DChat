9dcde996442c8785387f7599591aea4b5549ee11
dreonwo <dwheatleyowens@icloud.com>
2023-02-08 19:39:44 -0500

    Updated Color Scheme and made small UI Adjustments

--- main.js
+++ main.js
@@ -147,7 +147,7 @@ window.onload = ()=>{
             else
                 msgDiv.classList.add('theirMsg');
     
-            senderSpan.textContent = data.sender;
+            //senderSpan.textContent = data.sender;
             senderSpan.classList.add('sender');
     
             timeSpan.classList.add('time');
